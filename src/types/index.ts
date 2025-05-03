ae88bb162556d44c5fa593ecba2d949eaa9c47fa
Nikita-Tig <nikitatig2005@gmail.com>
2025-05-03 23:04:14 +0300

    reworked all the documentations and interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,72 +1,21 @@
-import { IEvents } from "../components/base/events";
-
-interface ApiProduct {
-    id: string;
-    description: string;
-    image: string;
-    title: string;
-    category: string;
-    price: number;
-}
-
-interface Payment {
-    onlinePayment: boolean;
-    adress: string;
-}
-
-interface Contacts {
-    email: string;
-    phone: string;
-}
-
-interface Oreder extends Contacts {
-    products: ApiProduct[];
-}
-
-interface OrederReult extends ApiProduct {
-    id: string;
-}
-
-interface CardData {
-    category: string;
-    title: string;
-    description: string;
-    image: string;
-    price: number;
-}
-  
-interface CompactCardData {
-    title: string;
-    price: number;
-}
-
-interface IBaseModal {
-    item: HTMLElement;
-    openModal(id: string): void;
-    closeModal(id: string): void;
-}
-
-interface IBasketModel {
-    items: Map<string, number>;
-    add(id: string): void;
-    remove(id: string): void;
-}
-
 interface IProduct {
     id: string;
+    description: string;
+    image: string;
     title: string;
+    category: string;
+    price: number;
 }
 
-interface ICatalogModel {
-    items: IProduct[];
-    setItems(items: IProduct[]): void;
-    getProduct(id: string): IProduct;
+interface IShop {
+    cardsList: HTMLElement[];
 }
 
-interface IViewConstructor {
-    new (container: HTMLElement, events?: IEvents): IView;
+interface IBasket {
+    itemsList: HTMLElement[];
+    itemsTotal: number;
 }
 
-interface IView {
-    render(data?: object): HTMLElement;
+interface IPayment {
+    value: string;
 }
